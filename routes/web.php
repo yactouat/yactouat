9954354dabf4c480d5c548e22fe9d2230b490af2
yactouat <yactouat@hotmail.com>
2023-12-05 21:39:15 +0100

    investigating the unsubscribe link issue on prod

--- routes/web.php
+++ routes/web.php
@@ -57,15 +57,20 @@ Route::get('test-start', function (Request $request) {
 })->middleware(AllowsOnlyAdmin::class);
 Route::get('test-end', function (Request $request) {
     
-    // get signed route from db
-    $signedRouteService = resolve('SignedRouteService');
-    $persistedSignedRoute = $signedRouteService->fetch($request);
+    try {
+        // get signed route from db
+        $signedRouteService = resolve('SignedRouteService');
+        $persistedSignedRoute = $signedRouteService->fetch($request);
 
-    // get params from signed route
-    $parsed = parse_url($request->fullUrl());
-    parse_str($parsed['query'], $queryParameters);
-    // it's a dirty job but someone's gotta do it
-    $signature = $queryParameters["amp;signature"];
+        // get params from signed route
+        $parsed = parse_url($request->fullUrl());
+        parse_str($parsed['query'], $queryParameters);
+        // it's a dirty job but someone's gotta do it
+        $signature = $queryParameters["amp;signature"];
 
-    dd($persistedSignedRoute, $parsed, $queryParameters, $signature);
+        dd($persistedSignedRoute, $parsed, $queryParameters, $signature);        
+    } catch (\Throwable $th) {
+        dd($th);
+    }
+
 })->middleware(AllowsOnlyAdmin::class);
